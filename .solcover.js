f5cd11283fd01423d679c4dd5a15b9f08c08dc63
redDwarf03 <sylvain@archethic.net>
2025-10-07 16:25:35 +0200

    Update Slither configuration files to include 'echidna' in filter paths and skip files. This change enhances security analysis by excluding additional directories from checks, improving overall code quality and maintainability.

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,5 @@
 // .solcover.js
 module.exports = {
-    skipFiles: [
-        "mocks",
-    ],
+    skipFiles: ["mocks", "echidna"],
     istanbulReporter: ["text", "lcov"], // lcov for CI (Codecov/Coveralls), text for the console
 };
